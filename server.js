1c4cbbaf917b242e4e771fd4cda3a2b112c7c382
sshodges <sshodges@gmail.com>
2019-08-18 12:46:56 +1000

    Add heroku deployment details

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const connectDB = require('./config/db');
-var cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 //Cors Settings
 var corsOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST, DELETE, OPTIONS',
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST, DELETE, OPTIONS",
   preflightContinue: true,
   optionsSuccessStatus: 204,
-  exposedHeaders: 'x-auth-token'
+  exposedHeaders: "x-auth-token"
 };
 
 app.use(cors(corsOptions));
@@ -21,18 +22,28 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to contact keeper API...' })
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to contact keeper API..." })
 );
 
 //Define Routes
-app.use('/api/users', require('./routes/users.js'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users.js"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assets in production
+if ((process.env.NODE_ENV = "production")) {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (res, req) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server started on port: ' + PORT));
+app.listen(PORT, () => console.log("Server started on port: " + PORT));
 
 // TODO
 // -  email lowercase
